00d33a03afba61c689acbe6d3330dc453e62039e
Volodymyr Shcherbinin (vovin) <vovin@lurk.kiev.ua>
2021-12-19 23:19:07 +0200

    Update artifact location

--- Formula/hecate.rb
+++ Formula/hecate.rb
@@ -1,7 +1,7 @@
 class Hecate < Formula
   desc "This is 'The Hex Editor From Hell!'"
   homepage "https://github.com/evanmiller/hecate"
-  url "https://github.com/kaos/homebrew-shell/releases/download/hecate-2021-12-19/hecate_2021-12-19_Darwin_x86_64.tar.gz"
+  url "https://github.com/kaos/homebrew-shell/releases/download/hecate-2021-12-19/hecate-2021-12-19_Darwin_x86_64.tar.gz"
   sha256 "49418e04420534e4588f245e6c3a16733ff4411fb909945b932b082fc1c3599e"
 
   test do
